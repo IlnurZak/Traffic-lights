046795c7a3b784a6764fde58eec8cf5cde9ca391
Ilnur <zakirynov.ilnur.2002@gmaIL.com>
2022-03-07 06:08:23 +0500

    Finally stackView

--- Traffic lights/ViewController.swift
+++ Traffic lights/ViewController.swift
@@ -9,15 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var colorRedView: UIView!
-    @IBOutlet var colorYellowView: UIView!
-    @IBOutlet var colorGreenView: UIView!
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        colorRedView.layer.cornerRadius = 75
-        colorYellowView.layer.cornerRadius = 75
-        colorGreenView.layer.cornerRadius = 75
+        
     }
 
 
